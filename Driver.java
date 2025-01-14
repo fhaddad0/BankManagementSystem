4022d4312fdfd101ae1132f71a73907924a86f34
fhaddad0 <ferashaddad2000@gmail.com>
2025-01-14 14:12:47 +0000

    Menu for accessing commands for account

--- Driver.java
+++ Driver.java
@@ -1,7 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Driver {
+    private ArrayList<BankAccount> accounts = new ArrayList<>();
 
     public static void main(String[] args) {
-        BankAccount bank1 = new BankAccount("Jim", 1, 45);
-        System.out.println(bank1.displayAccountDetails());
+        
+    }
+
+    public static void menu(){
+        System.out.println("Welcome to the Bank Management System");
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            System.out.println("""
+                    Instructions for use:
+                    --- 1: Create a new bank account
+                    --- 2: Deposit money
+                    --- 3: Withdraw money
+                    --- 4: View account details
+                    --- 0: Exit Menu
+                    
+                    Please enter an option: """);
+
+            int option = input.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.println("Please enter your name: ");
+                    String name = input.next();
+                    System.out.println("Please enter your address: ");
+
+            }
+        }
     }
 }
